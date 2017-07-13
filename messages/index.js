dfa4672c725ead3b5e0b66f7f3f6799fc61b9e8b
Jared Matfess <jaredm@slalom.com>
2017-07-12 21:35:44 -0400

    Adding LUIS

--- messages/index.js
+++ messages/index.js
@@ -21,25 +21,26 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
 var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
 
+var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+bot.recognizer(recognizer);
+
 bot.dialog('/', [
     function (session) {
-        builder.Prompts.text(session, "Hey there... What's your name?");
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
-    },
-    function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
-    },
-    function (session, results) {
-        session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name + 
-                    " you've been programming for " + session.userData.coding + 
-                    " years and use " + session.userData.language + ".");
+        builder.Prompts.text(session, "Hey there... How can I make your day amazing?");
     }
 ]);
+
+
+bot.dialog('PasswordReset', [
+    function (session, args, next) {
+        var answer = intentAnswers['Password Reset'];
+        builder.Prompts.text("Password reset?");
+        session.send(answer);
+
+    }
+]).triggerAction({
+    matches: 'Password Reset'
+});
 
 if (useEmulator) {
     var restify = require('restify');
