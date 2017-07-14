bbdaf44cb71faeecae2cb9672255e2e99fafeecd
Jared Matfess <jaredm@slalom.com>
2017-07-14 08:14:31 -0400

    Tweaks

--- messages/index.js
+++ messages/index.js
@@ -7,8 +7,22 @@ https://aka.ms/abs-node-waterfall
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var path = require('path');
+var spbot = require('./spbot.js');
+var restify = require('restify');
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+// Create connector and listen for messages
+var connector = new builder.ChatConnector({
+    appId: "446d96da-69c2-4313-a499-e1c555c983f8",
+    appPassword: "xVn76K1K7oYWzdW7WbAGKOJ"
+});
+server.post('/api/messages', connector.listen());
 
-
+//Uncomment for Prod
+/*
 var useEmulator = (process.env.NODE_ENV == 'development');
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
@@ -17,25 +31,32 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
     stateEndpoint: process.env['BotStateEndpoint'],
     openIdMetadata: process.env['BotOpenIdMetadata']
 });
-
+*/
 var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
 
-var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+var recognizer = new builder.LuisRecognizer("https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/d4c7b802-b6e8-48f7-9203-d20029763f2c?subscription-key=57eeb6245eee4b019fb729679ab72863&timezoneOffset=0&verbose=true&q=");
 
 bot.recognizer(recognizer);
 
-bot.dialog('/', [
-    function (session) {
-        builder.Prompts.text(session, "Hey there... How can I make your day amazing?");
-    }
-]);
+var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+    .matches('PasswordReset', [
+        (session, args) => {
+            session.send(spbot.PasswordReset).endDialog();
+        }
+    ])
+    .matches('OrderSoftware', [
+        (session, args) => {
+            session.send(spbot.OrderSoftawre).endDialog();
+        }
+    ])
+    .onDefault((session) => {
+        session.send(spbot.FallbackIntentMessage).endDialog();
+    });
 
+bot.dialog('/', intents);
 
-bot.dialog('Password Reset', function (session) {
-    session.endDialog("This bot will echo back anything you say. Say 'goodbye' to quit.");
-}).triggerAction({ matches: 'Password Reset' });
-
+/*
 if (useEmulator) {
     var restify = require('restify');
     var server = restify.createServer();
@@ -45,4 +66,4 @@ if (useEmulator) {
     server.post('/api/messages', connector.listen());    
 } else {
     module.exports = { default: connector.listen() }
-}
+}*/
---
+++ messages/spbot.js
@@ -0,0 +1,5 @@
+module.exports = {
+    FallbackIntentMessage: "Hmm... I did not understand that. Rephrase your message and try again.",
+    PasswordReset: "So you want to reset your password eh?",
+    OrderSoftware: "What software would you like to order?"
+}
